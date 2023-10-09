cb3862e88fc8832765a57e9e38ecc88ca30967c3
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-10-09 12:20:30 -0400

    Finish final working model with user input

--- script.js
+++ script.js
@@ -19,35 +19,40 @@ for (i = 0; i < 16; i++) {
     
 };
 
+function generateNewGrid (userInput) {
+   
+        
+        for (i = 0; i < userInput; i++) {
+    
+            for (j = 0; j < userInput; j++) {
+                                        
+                const square = document.createElement('div');
+                square.classList.add('square');
+                
+                square.style.width = (100/userInput) + '%';
+                square.style.height = (100/userInput) + '%';
+                
+                square.addEventListener('mouseover',() => {
+                    square.style.backgroundColor = 'black';
+        
+                    setTimeout(() => {
+                        square.style.backgroundColor = '';
+                    }, 500);
+                });
 
+                container.appendChild(square);
+            };
+        };
+    
+};
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     const userInput = prompt('Number of squares per side');
-    (function generateNewGrid () {
-        if (userInput > 100) {
-            alert('ERROR!Number must not be greater than 100');
-        } else if(userInput <= 100) {
-            for (i = 0; i < userInput; i++) {
-        
-                for (j = 0; j < userInput; j++) {
-                    
-                    const square = document.createElement('div');
-
-                    square.classList.add('square');
-                    square.style.width = (100/userInput) + '%';
-                    square.style.height = (100/userInput) + '%';
-
-                    container.appendChild(square);
-                    square.addEventListener('mouseover',() => {
-                        square.style.backgroundColor = 'black';
-            
-                        setTimeout(() => {
-                            square.style.backgroundColor = '';
-                        }, 500);
-                    });
-                };
-            };
-        };
-    })();
+    if (userInput > 100) {
+        alert('ERROR!Number must not be greater than 100');
+    } else if(userInput <= 100) {
+        container.innerHTML = '';
+        generateNewGrid(userInput);
+    }
 });
